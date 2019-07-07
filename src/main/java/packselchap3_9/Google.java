3e3f61ef23e34be6896dc663456e42f4d6b4faeb
sandeshpp <samklus@gmail.com>
2019-07-07 19:20:58 -0400

    correct title title

--- src/main/java/packselchap3_9/Google.java
+++ src/main/java/packselchap3_9/Google.java
@@ -20,7 +20,7 @@ public class Google {
 //<<<<<<< HEAD
    //     if (!driver.getTitle().equals("Google-Selenium Testing Tools")){
 //=======
-        if (!driver.getTitle().equals("Google-test")){
+        if (!driver.getTitle().equals("Google")){
 //>>>>>>> 68fa745cb5b2607eeded53d187bddd54d183c832
             throw new WrongPageException("Incorrect page for Google Home page");
         }
